9fd1f29f2471bc9d8fecb8cdcc597caeecb7d4ef
Yjn024 <61787533+JieningYu@users.noreply.github.com>
2023-02-10 12:08:02 +0800

    e

--- src/main/java/com/dm/earth/conitator/impl/client/ClientInit.java
+++ src/main/java/com/dm/earth/conitator/impl/client/ClientInit.java
@@ -1,10 +1,12 @@
 package com.dm.earth.conitator.impl.client;
 
 import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.loader.api.minecraft.ClientOnly;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 
 import com.dm.earth.conitator.impl.client.events.ClientInitCallback;
 
+@ClientOnly
 public class ClientInit implements ClientModInitializer {
 
 	@Override
