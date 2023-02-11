0160a16ae850887c6d86acaf3081f170022bb5dc
Yjn024 <61787533+JieningYu@users.noreply.github.com>
2023-02-11 09:55:58 +0800

    safe test

--- src/main/java/com/dm/earth/conitator/ConitatorMod.java
+++ src/main/java/com/dm/earth/conitator/ConitatorMod.java
@@ -1,16 +1,35 @@
 package com.dm.earth.conitator;
 
 import org.jetbrains.annotations.ApiStatus.Internal;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 
+import com.dm.earth.conitator.test.ConitatorTestMod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.minecraft.util.Identifier;
 
 @Internal
-public class ConitatorMod {
+public class ConitatorMod implements ModInitializer, DataGeneratorEntrypoint {
 
 	public static final String MOD_ID = "conitator";
+	public static final boolean DEBUG = true;
 
 	public static Identifier id(String name) {
 		return new Identifier(MOD_ID, name);
+	}
+
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		if (DEBUG)
+			ConitatorTestMod.onInitializeDataGenerator(fabricDataGenerator);
+	}
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+		if (DEBUG)
+			ConitatorTestMod.onInitialize(mod);
 	}
 
 }
--- src/main/java/com/dm/earth/conitator/test/ConitatorTestMod.java
+++ src/main/java/com/dm/earth/conitator/test/ConitatorTestMod.java
@@ -1,7 +1,6 @@
 package com.dm.earth.conitator.test;
 
 import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.quiltmc.qsl.block.extensions.api.QuiltBlockSettings;
 
 import com.dm.earth.conitator.ConitatorMod;
@@ -9,16 +8,14 @@ import com.dm.earth.conitator.api.Conitator;
 import com.dm.earth.conitator.api.DefaultEntryKeys;
 import com.dm.earth.conitator.api.builder.ConitatorBlockBuilder;
 
-import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.tag.BlockTags;
 
-public class ConitatorTestMod implements ModInitializer, DataGeneratorEntrypoint {
+public class ConitatorTestMod {
 
-	public static final boolean DEBUG = true;
 	public static Conitator conitator = Conitator.create().apply(DefaultEntryKeys.block(), DefaultEntryKeys.item(),
 			DefaultEntryKeys.blockTag(), DefaultEntryKeys.blockLootTable(), DefaultEntryKeys.model(),
 			DefaultEntryKeys.translation("en_us"), DefaultEntryKeys.itemTag());
@@ -31,13 +28,10 @@ public class ConitatorTestMod implements ModInitializer, DataGeneratorEntrypoint
 			.model((g, i, b) -> g.registerSimpleCubeAll(b))
 			.get();
 
-	@Override
-	public void onInitialize(ModContainer mod) {
-
+	public static void onInitialize(ModContainer mod) {
 	}
 
-	@Override
-	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+	public static void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		conitator.loadDatagen(fabricDataGenerator);
 	}
 
